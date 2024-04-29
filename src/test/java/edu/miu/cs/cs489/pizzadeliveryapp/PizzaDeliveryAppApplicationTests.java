4bad5ab6d63fd2de1c5f0ed9ca354ec08025e801
MayAlejita <92763601+MayAlejita@users.noreply.github.com>
2024-04-28 22:33:16 -0500

    Update PizzaDeliveryAppApplicationTests.java

--- src/test/java/edu/miu/cs/cs489/pizzadeliveryapp/PizzaDeliveryAppApplicationTests.java
+++ src/test/java/edu/miu/cs/cs489/pizzadeliveryapp/PizzaDeliveryAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class PizzaDeliveryAppApplicationTests {
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
 }
