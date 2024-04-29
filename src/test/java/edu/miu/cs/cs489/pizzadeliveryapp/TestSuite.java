d54f0275c8a9c912a4ad5f64367e421501fbca75
MayAlejita <92763601+MayAlejita@users.noreply.github.com>
2024-04-28 22:32:57 -0500

    Update TestSuite.java

--- src/test/java/edu/miu/cs/cs489/pizzadeliveryapp/TestSuite.java
+++ src/test/java/edu/miu/cs/cs489/pizzadeliveryapp/TestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
-@SuiteClasses({ CustomerServiceImplTest.class, PizzaServiceImplTest.class })
+//@RunWith(Suite.class)
+//@SuiteClasses({ CustomerServiceImplTest.class, PizzaServiceImplTest.class })
 public class TestSuite {
 }
